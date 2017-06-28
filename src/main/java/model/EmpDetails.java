fc3ca59204d2bad57922bdb36f08e1ebd595a361
Lakshay Suri <lakshay.suri@gemalto.com>
2017-06-28 16:18:55 +0200

    feature(model): Added the java pojo for the cassandra unit project.

---
+++ src/main/java/model/EmpDetails.java
@@ -0,0 +1,17 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author lakshay13@gmail.com
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmpDetails {
+
+    int id;
+    String name;
+}
